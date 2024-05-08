bc10070a90bb95d11aac39d150522175d3d45b7f
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-05-09 02:25:13 +0530

    Update Message Form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,34 @@
-const MessageForm = () => {
+import { useState } from "react"
+import { sendMessage, isTyping } from "react-chat-engine";
+
+const MessageForm = (props) => {
+    const [value, setValue] = useState('');
+    const {chatID, creds} = props;
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length > 0) sendMessage(creds, chatID, {text});
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+
+        isTyping(props, chatID);
+    }
+    
     return (
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input 
+                className="message-input"
+                placeholder="Send a message ..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     )
 }
 
